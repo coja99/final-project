765f14c3bdba1662658be30885ce0b8dc68bbb0a
Coja <jovanjovanjovanovic1999@gmail.com>
2022-01-27 22:08:21 +0100

    dodate fje na bazi i index promenjen, dodate nove stranice ali treba da se dorade

---
+++ login/index.php
@@ -0,0 +1,56 @@
+<?php
+include_once('./includes/baza.php');
+session_start();
+
+
+//ako korisnik vec postoji u sesiji
+if(isset($_SESSION['user'])) {
+    header('Location: ./index.php');
+}
+
+//ako korisnik vec postoji u cookies
+if(isset($_COOKIE['user'])) {
+    $_SESSION['user'] = $_COOKIE['user'];
+    header('Location: ./index.php');
+}
+
+//ako se korisnik upravo ulogovao
+if(isset($_POST['user']) && isset($_POST['pass'])) {
+    if($connection->proveriKorisnika($_POST['user'],$_POST['pass'])) {
+        //ako je checkiran keep me logged in
+        if(isset($_POST['keep'])) {
+            setcookie("user",$_POST['user'],time()+60*60*24);
+        }
+        $_SESSION['user'] = $_POST['user'];
+        header('Location: ./index.php');
+    }
+    $greska = true;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="POST" action="./login.php">
+        <label for="user" >Username: </label>
+        <input type="email" id="user" name="user" />
+        <br>
+        <label for="pass" >Password: </label>
+        <input type="password" id="pass" name="pass" />
+        <br>
+        <label for="keep" >Keep me logged in: </label>
+        <input type="checkbox" name="keep" id="keep" /> 
+        <br> 
+        <input type="submit" value="Login" />
+    </form>
+    <?php if(isset($greska) && $greska) :?>
+        <div id='greska'>Pogrešan unos. Proveri lozinku ili korisničko ime.</div>
+    <?php endif; ?>
+</body>
+</html>
--- oglas.php
+++ oglas.php
@@ -1,7 +1,33 @@
-<?php include "./includes/baza.php";
-        
+<?php 
+include_once('./includes/baza.php');
+
+session_start();
+class Korpa {
+    function __construct() {
+        if(!isset($_SESSION['item_cart'])) { 
+            $_SESSION['item_cart'] = [];
+        }
+    }
+
+    function dodajUKorpu($id,$amount) {
+        if(isset($_SESSION['item_cart'][$id])) {
+            $_SESSION['item_cart'][$id] += $amount;
+        } else {
+            $_SESSION['item_cart'][$id] = $amount;
+        }
+    }
+
+    function listajKorpu() {
+        foreach ($_SESSION['item_cart'] as $id => $amount) {
+            echo "Stavka $id je u korpi $amount puta<br>";
+        }
+    }
+
+    function isprazniKorpu() {
+        $_SESSION['item_cart'] = [];
+    }
+}
 
 
-
-
+$korpa = new Korpa();
 ?>
